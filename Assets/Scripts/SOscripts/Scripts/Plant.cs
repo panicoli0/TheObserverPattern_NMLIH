f3f3352186e19377c7a2850a8bb4ccb583e5d72c
Pablo Nicoli <61359259+panicoli0@users.noreply.github.com>
2021-10-11 19:23:43 +0400

    Initial Project Structure
    All assets needed and main scene to achieve the tutorial.

---
+++ Assets/Scripts/SOscripts/Scripts/Plant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField] private PlantData info;
+
+    private SetPlantInfo spi;
+
+    private void Start()
+    {
+        spi = GameObject.FindWithTag("PlantInfo").GetComponent<SetPlantInfo>();
+    }
+
+    private void OnMouseDown()
+    {
+        spi.OpenPlantPanel();
+        spi.plantName.text = info.Name;
+        spi.threatLevel.text = info.Threat.ToString();
+        spi.plantDescription.text = info.Description;
+        spi.plantIcon.GetComponent<RawImage>().texture = info.Icon;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player") && info.Threat == PlantData.THREAT.High)
+        {
+            // We can call it like that because dead is a static variable
+            PlayerController.dead = true;
+            FindObjectOfType<PlayerHealth>().PlayerHP = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/WorldManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+    private const int Z_ORIGIN_POSITION = -27;
+    private const int X_ORIGIN_POSITION = -92;
+
+    public GameObject[] trees;
+    public Terrain terrain;
+    public Transform plantsContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlantTrees();
+    }
+
+    void PlantTrees()
+    {
+        TerrainData terrainData = terrain.terrainData;
+        int treeSpacing = 10;
+        for (int z = 0; z < terrainData.size.z; z += treeSpacing)
+        {
+            for (int x = 0; x < terrainData.size.x; x += treeSpacing)
+            {
+                Vector3 pos = new Vector3(x, 0, z);
+                pos.y = terrain.SampleHeight(pos);
+                int i = Random.Range(0, trees.Length);
+                GameObject plants = Instantiate(trees[i], pos, Quaternion.identity);
+                plants.transform.SetParent(plantsContainer);
+                
+            }
+        }
+    }
+}
